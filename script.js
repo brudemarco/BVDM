d71d7f74e964b2da816622d10cea8199b5b65335
Bruna Vignola de Marco <brudemarco@gmail.com>
2020-07-27 00:31:57 -0300

    rain_commit

--- script.js
+++ script.js
@@ -63,3 +63,22 @@ const init = () => {
 
 init();
 
+
+
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
