c4f6ecf1dde2c2579b40806c53b5d6c735fbca44
Bruna Vignola de Marco <brudemarco@gmail.com>
2020-07-29 00:14:50 -0300

    ultimopash

--- script.js
+++ script.js
@@ -82,3 +82,22 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
+
+
+
+//boton final//
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
